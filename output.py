8741ae5932014db248b165f67bdc0c880995ddaa
J Corbett <jcorb8800@gmail.com>
2021-03-17 15:59:00 +0000

    Call function

--- output.py
+++ output.py
@@ -4,6 +4,7 @@ import webbrowser
 class Output:
     def __init__(self, link):
         self.url = link
+        self.output_url()
 
     def output_url(self):
         webbrowser.open(self.url, new=2, autoraise=True)
