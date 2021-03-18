b6112d6a3d7e5c67198a4c833fb9bfcea8a6c022
J Corbett <jcorb8800@gmail.com>
2021-03-18 13:24:52 +0000

    max results 2

--- duckduckgo.py
+++ duckduckgo.py
@@ -8,17 +8,14 @@ from mycroft.util.log import LOG
 # prints out list of urls
 # edit to give descriptions too?
 def search(keywords, max_results=None):
-    url = 'https://html.duckduckgo.com/html/q'
-    params = {
-        'q': keywords,
-        's': '0',
-    }
+    url = 'https://html.duckduckgo.com/html/'
+    params = {'q': keywords}
 
     yielded = 0
     while True:
-        res = requests.post(url, data=keywords)
+        res = requests.post(url, data=params)
         doc = html.fromstring(res.text)
-        #LOG.debug("html", res.text)
+        LOG.debug("html", res.text)
 
         results = [a.get('href') for a in doc.cssselect('#links .links_main a')]
         LOG.debug("result", len(results))
