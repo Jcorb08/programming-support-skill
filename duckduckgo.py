e690a2a5b5740e002981f77d0568caa63a9753e3
J Corbett <jcorb8800@gmail.com>
2021-03-18 12:59:18 +0000

    max results 2

--- duckduckgo.py
+++ duckduckgo.py
@@ -20,7 +20,7 @@ def search(keywords, max_results=None):
         doc = html.fromstring(res.text)
 
         results = [a.get('href') for a in doc.cssselect('#links .links_main a')]
-        LOG.debug(results[0])
+        LOG.debug("result", results[0])
         for result in results:
             yield result
             time.sleep(0.1)
