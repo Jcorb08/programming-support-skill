999bb84d8ca0c071cf65f47d687bc77dd563f352
J Corbett <jcorb8800@gmail.com>
2021-03-18 13:46:04 +0000

    fixed url error

--- duckduckgo.py
+++ duckduckgo.py
@@ -15,9 +15,10 @@ def search(keywords, max_results=None):
     while True:
         res = requests.post(url, data=params)
         doc = html.fromstring(res.text)
+        LOG.debug("res", res.text)
 
         results = [a.get('href') for a in doc.cssselect('#links .links_main a')]
-        LOG.debug("results", results[0])
+        LOG.debug("results", len(results))
         for result in results:
             yield result
             time.sleep(0.1)
