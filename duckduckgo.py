66f87a81d6d54538706afa0722fffdef76c6f228
J Corbett <jcorb8800@gmail.com>
2021-03-18 16:40:53 +0000

    fixed url error

--- duckduckgo.py
+++ duckduckgo.py
@@ -10,10 +10,10 @@ from mycroft.util.log import LOG
 def search(keywords, max_results=None):
     url = 'https://duckduckgo.com/'
     params = {'q': keywords}
-
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0'}
     yielded = 0
     while True:
-        res = requests.post(url, data=params)
+        res = requests.post(url, data=params, headers=headers)
         doc = html.fromstring(res.text)
         LOG.debug("res", res.text)
 
