7ac620a6b13fdce614440e2a68aa4d0075b3e81d
J Corbett <jcorb8800@gmail.com>
2021-04-09 21:25:30 +0100

    get rid of current utterance

--- __init__.py
+++ __init__.py
@@ -54,8 +54,9 @@ class ProgrammingSupport(CommonQuerySkill):
                     LOG.debug(action_.scores, "sorted scores")
                     self.speak('Similar utterances include:')
                     for score in action_.scores:
-                        self.speak(score[1][0] + ' of class ' + score[0] + ' with a score of ' +
-                                   str(round(score[1][1], 2)))
+                        if score[0] == action_.resource:
+                            self.speak(score[1][0] + ' of class ' + score[0] + ' with a score of ' +
+                                       str(round(score[1][1], 2)))
                     # action_.scores
                     # self.ask_selection()
                     # sort them and speak them out
